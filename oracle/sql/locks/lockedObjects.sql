cf40828398a793bf97d3fe6fa16fda50e74f349c
alamer <shherbakoviv@gmail.com>
2019-10-30 09:15:34 +0300

    add second locked objects script

--- oracle/sql/locks/lockedObjects.sql
+++ oracle/sql/locks/lockedObjects.sql
@@ -1,2 +1,21 @@
 SELECT B.Owner, B.Object_Name, A.Oracle_Username, A.OS_User_Name  
 FROM V$Locked_Object A, All_Objects B
+
+
+select
+   c.owner,
+   c.object_name,
+   c.object_type,
+   b.sid,
+   b.serial#,
+   b.status,
+   b.osuser,
+   b.machine
+from
+   v$locked_object a ,
+   v$session b,
+   dba_objects c
+where
+   b.sid = a.session_id
+and
+   a.object_id = c.object_id;
