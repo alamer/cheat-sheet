f5b9ea226168b23b1efe8d93bc58b92d7a283155
alamer <antilamer87@mail.ru>
2020-07-28 18:57:05 +0800

    add(ora): free tablespace size from dba_data_files

--- oracle/sql/tablespace/getFreeSpaceByTablespace.sql
+++ oracle/sql/tablespace/getFreeSpaceByTablespace.sql
@@ -1,3 +1,9 @@
 select TABLESPACE_NAME, round( sum( BYTES ) / 1024 / 1024 / 1024, 1 ) gb 
 from user_free_space
 group by TABLESPACE_NAME
+
+
+select TABLESPACE_NAME,sum(bytes)/1024/1024/1024 as allocated_gb,sum(MAXBYTES)/1024/1024/1024 as total_gb,
+     sum(MAXBYTES-bytes)/1024/1024/1024 as free_gb
+from DBA_DATA_FILES
+group by TABLESPACE_NAME
